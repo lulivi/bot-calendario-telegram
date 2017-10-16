115da8439a08fa59b257ab821aa194deb9b0f98a
lulivi <luislivilla@gmail.com>
2017-10-16 23:07:25 +0200

    Added setup file closes #7

---
+++ setup.py
@@ -0,0 +1,22 @@
+"""Setup file."""
+
+# external imports
+from setuptools import setup
+
+# long description
+with open("README.md", 'r') as f:
+    long_description = f.read()
+
+# requirements
+requirements = ['sqlite3']
+
+setup(
+    name='bot-calendario-telegram',
+    version='0.2',
+    description='A telegram bot to keep every objective up to date.',
+    long_description=long_description,
+    license="GPLv3",
+    author='Luis Liñán',
+    author_email='luislivilla@gmail.com',
+    url="https://github.com/lulivi/bot-calendario-telegram",
+    install_requires=requirements, )
