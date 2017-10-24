36b74a81e8733f949aa7271e279386f8a6791fde
lulivi <luislivilla@gmail.com>
2017-10-24 12:33:51 +0200

    Modified the estructure of Reminder. Now using orm (peewee). closes #12

--- bot_calendario_telegram/reminder.py
+++ bot_calendario_telegram/reminder.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python
 # -*- coding: UTF-8 -*-
 """
 Remainder python module.
@@ -7,77 +6,65 @@ Checks in a data base when an event needs to be reminded.
 """
 
 # External imports
-import sqlite3
+import reminder_database_model as db_model
+import peewee
 
 
 # === Reminder Class ===
-class Reminder:
+class Reminder(object):
     """
-    Checks and modifies a data base for geting and saving events.
+    Checks and modifies a database.
 
     Methods:
 
-    * save_event
-    * get_event
-    * delete_event
-    * next_event
-    * get_all_events
+    * initialize - Initialize the database object.
+    * connect - Connect to the database.
+    * close - Close the connection.
+    * save_event - Save the event in the data base.
+    * update_event - Updates an existing event.
+    * get_event - Get the event from the data base.
+    * delete_event - Delete event from Database.
+    * next_event - Return nearest event.
+    * get_all_events - Return all events sorted by date.
     """
 
-    __db_connection = None
-    __db_cursor = None
-    __db_file = None
-
-    # === Constructor ===
-    def __init__(self, db_file):
+    # === Initialize connection ===
+    def initialize(self, db_name):
         """
-        Constructor.
+        Initialize the database object.
 
-        Asign the data base path to a private variable.
+        Sets the database name for connecting to it later
 
         Args:
 
-        * db_file - Data base path
+        * db_name - The database name
         """
-        self.__db_file = db_file
+        db_model.database.init(db_name)
 
-    # === Open connection ===
-    def _open_connection(self):
+    # === Connect ===
+    def connect(self):
         """
         Connect to the database.
 
-        Creates a cursor from the connection to iterate and execute query
-        sentences.
+        Starts the connection to perform queris to the database.
         """
-        try:
-
-            self.__db_connection = sqlite3.connect(self.__db_file)
-            self.__db_connection.row_factory = sqlite3.Row
-            self.__db_cursor = self.__db_connection.cursor()
-
-        except Exception as e:
-            raise
+        db_model.database.connect()
 
     # === Close connection ===
-    def _close_connection(self):
+    def close(self):
         """
         Close the connection.
 
-        Simple function to close the connection.
+        Closes the connection after the database use.
         """
-        try:
-
-            self.__db_connection.close()
-
-        except Exception as e:
-            raise
+        db_model.database.close()
 
     # === Save event ===
-    def save_event(self, id_event, event_date, preserve=True):
+    def save_event(self, id_event, date_event):
         """
         Save the event in the data base.
 
-        Perform a query sentence to save the event in the data base.
+        Performs a query sentence to save the event in the data base.
 
         Args:
 
@@ -86,29 +73,69 @@ class Reminder:
 
         Returns:
 
-        * -1 If there was any exception
-        *  0 If everything were correct
+        * The inserted object if everything went ok
         """
         try:
-            self._open_connection()
-
-            self.__db_cursor.execute('INSERT INTO EVENTS VALUES(?, ?)',
-                                     (id_event, event_date))
-            if preserve:
-                self.__db_connection.commit()
-
-            self._close_connection()
-        except Exception as e:
-            return -1
+            insertion = db_model.Event.create(
+                id_event=id_event, date_event=date_event)
+        except peewee.IntegrityError as e:
+            raise
         else:
-            return 0
+            return {insertion.id_event: insertion.date_event}
+
+    def update_event(self, id_event, new_date_event):
+        """
+        Update an existing event.
+
+        Performs an update query to to an existing event of the database
+
+        Args:
+
+        * id_event - The existing event id
+        * new_date_event - The updated date
+
+        Returns:
+
+        * result - Number of rows updated (should be 1)
+        """
+        try:
+            update = db_model.Event.update(date_event=new_date_event).where(
+                db_model.Event.id_event == id_event)
+            result = update.execute()
+        except Exception as e:
+            raise
+        else:
+            return result
+
+    # === Delete event ===
+    def delete_event(self, id_event):
+        """
+        Delete event from Database.
+
+        Performs a query sentence to remove a tuple from the database.
+
+        Args:
+
+        * id_event - ID of the event
+
+        Returns:
+
+        * result - Number of rows deleted (should be 1)
+        """
+        try:
+            result = db_model.Event.delete().where(
+                db_model.Event.id_event == id_event).execute()
+        except Exception as e:
+            raise
+        else:
+            return result
 
     # === Get event ===
     def get_event(self, id_event):
         """
         Get the event from the data base.
 
-        Perform a query sentence to get the event from the data base.
+        Performs a query sentence to get the event from the data base.
 
         Args:
 
@@ -116,105 +143,57 @@ class Reminder:
 
         Returns:
 
-        * -1 If there was any exception
-        * date_event If everything were correct
+        * selected - Dictionary of id_event: date_event as key: value
         """
         try:
-            self._open_connection()
-
-            selected_event = {}
-            selected_event = self.__db_cursor.execute(
-                'SELECT EventDate FROM EVENTS WHERE EventId = ?',
-                id_event).fetchone()
-
-            self._close_connection()
+            selected = dict(
+                [(id_event, date_event)
+                 for id, id_event, date_event in db_model.Event.select()
+                 .where(db_model.Event.id_event == id_event).tuples()])
         except Exception as e:
-            return -1
+            raise
         else:
-            return selected_event['EventDate']
-
-    # === Delete event ===
-    def delete_event(self, id_event, preserve=True):
-        """
-        Delete event from Database.
-
-        Perform a query sentence to remove a touple from the data base.
-
-        Args:
-
-        * id_event - ID of the event
-        * preserve - If it should execute the query (testing purposes)
-        DEFAULT: True
-
-        Returns:
-
-        * -1 If there was any exception
-        *  0 If everything were correct
-        """
-        try:
-            self._open_connection()
-
-            self.__db_cursor.execute(
-                'DELETE FROM EVENTS WHERE EventId = ?', id_event).fetchone()
-            if preserve:
-                self.__db_connection.commit()
-
-            self._close_connection()
-        except Exception as e:
-            return -1
-        else:
-            return 0
+            return selected
 
     # === Next event ===
     def next_event(self):
         """
-        Nearest event.
+        Return nearest event.
 
-        Perform a query sentence to get the nearest event from the data base.
+        Performs a query sentence to get the nearest event from the data base.
 
         Returns:
 
-        * -1 If there was any exception
-        * next_event If everything was fine
+        * selected - Dictionary of id_event: date_event as key: value
         """
         try:
-            self._open_connection()
-            next_event = self.__db_cursor.execute(
-                'SELECT * FROM EVENTS ORDER BY EventDate').fetchone()
-
-            self._close_connection()
-
-            dict_next_event = dict(next_event)
+            selected = dict(
+                [(id_event, date_event)
+                 for id, id_event, date_event in db_model.Event.select()
+                 .order_by(db_model.Event.date_event).limit(1).tuples()])
         except Exception as e:
-            return -1
+            raise
         else:
-            return dict_next_event
+            return selected
 
     # === Get all events ====
-    def get_all_events(self, date_sorted=True):
+    def get_all_events(self):
         """
         Return all events sorted by date.
 
-        Perform a query sentence to get every event from the data base.
-
-        Args:
-
-        * date_sorted - If want the results sorted by date DEFAULT: True
+        Performs a query sentence to get every event from the data base.
 
         Returns:
 
-        * -1 If there was any exception
-        * all_events If everything was fine
+        * events - Dictionaries of id_event: date_event as key: value for each
+        event
         """
         try:
-            self._open_connection()
-            all_events = self.__db_cursor.execute(
-                'SELECT * from EVENTS ORDER BY EventDate;').fetchall()
-
-            self._close_connection()
-
-            dict_all_events = dict(all_events)
+            events = dict(
+                [(id_event, date_event)
+                 for id, id_event, date_event in db_model.Event.select()
+                 .order_by(db_model.Event.date_event).tuples()])
         except Exception as e:
-            return -1
+            raise
         else:
-            return dict_all_events
+            return events
