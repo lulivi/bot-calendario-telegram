402423f2a0ada347e0862161a26d161d2e327df4
lulivi <luislivilla@gmail.com>
2017-11-14 22:22:13 +0100

    Added header with license

--- bot_calendario_telegram/__init__.py
+++ bot_calendario_telegram/__init__.py
@@ -1,5 +1,23 @@
-import sys
+# -*- coding: utf-8 -*-
+"""
+Define full path for a file used in this module.
+
+Copyright 2017, Luis Liñán (luislivilla@gmail.com)
+
+This program is free software: you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation, version 3.
+
+This program is distributed in the hope that it will be
+useful, but WITHOUT ANY WARRANTY; without even the implied
+warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+PURPOSE. See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>
+"""
 import os
+
 
 # Obtain current file directory
 working_dir = os.path.dirname(os.path.abspath(__file__))
--- bot_calendario_telegram/reminder_rest_api.py
+++ bot_calendario_telegram/reminder_rest_api.py
@@ -1,11 +1,20 @@
-"""rest api reminder python module.
+# -*- coding: utf-8 -*-
+"""
+Provide responses for some routes in the app.
 
-Functions:
+Copyright 2017, Luis Liñán (luislivilla@gmail.com)
 
-* get(/)
-* get(/all)
-* get(/number)
-* get(/get/{id})
+This program is free software: you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation, version 3.
+
+This program is distributed in the hope that it will be
+useful, but WITHOUT ANY WARRANTY; without even the implied
+warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+PURPOSE. See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>
 """
 # External imports
 import hug
