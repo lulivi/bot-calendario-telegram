9914f86ecc84df5f86958d147949af85d5807446
lulivi <luislivilla@gmail.com>
2017-11-15 22:06:51 +0100

    Beautify setup

--- setup.py
+++ setup.py
@@ -1,14 +1,17 @@
 """Setup file."""
 
-# external imports
-from setuptools import setup
+import os
+from setuptools import setup, find_packages
+from codecs import open
+
+root_path = os.path.dirname(os.path.abspath(__file__))
 
 # long description
-with open("README.md", 'r') as f:
+with open(os.path.join(root_path, "README.md"), encoding='utf-8') as f:
     long_description = f.read()
 
 # requirements
-with open('requirements.txt', 'r') as f:
+with open(os.path.join(root_path, 'requirements.txt'), encoding='utf-8') as f:
     requirements = list(filter(None, f.read().split('\n')))
 
 setup(
@@ -16,8 +19,9 @@ setup(
     version='0.4',
     description='A telegram bot to keep every objective up to date.',
     long_description=long_description,
-    license="GPLv3",
+    url="https://github.com/lulivi/bot-calendario-telegram",
     author='Luis Liñán',
     author_email='luislivilla@gmail.com',
-    url="https://github.com/lulivi/bot-calendario-telegram",
+    license="GPLv3",
+    packages=find_packages(exclude=['docs', 'tests', 'data']),
     install_requires=requirements, )
