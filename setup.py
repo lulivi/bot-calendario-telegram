a43ba796d787f70c283dd206d5e2a6ffc6084583
lulivi <luislivilla@gmail.com>
2017-11-28 16:08:27 +0100

    Test execution format change

--- setup.py
+++ setup.py
@@ -1,10 +1,31 @@
 """Setup file."""
 
 import os
+import sys
 from setuptools import setup, find_packages
+from setuptools.command.test import test as TestCommand
 from codecs import open
 
 root_path = os.path.dirname(os.path.abspath(__file__))
+
+
+class PyTest(TestCommand):
+    """Run test suite."""
+
+    def initialize_options(self):
+        """Initialize arguments to execute."""
+        TestCommand.initialize_options(self)
+        self.pytest_args = [
+            '-v', '--pylama', '--cov-report=term-missing',
+            '--cov=bot_calendario_telegram', 'tests/'
+        ]
+
+    def run_tests(self):
+        """Run the suite."""
+        import pytest
+        errno = pytest.main(self.pytest_args)
+        sys.exit(errno)
+
 
 # long description
 with open(os.path.join(root_path, "README.md"), encoding='utf-8') as f:
@@ -13,6 +34,12 @@ with open(os.path.join(root_path, "README.md"), encoding='utf-8') as f:
 # requirements
 with open(os.path.join(root_path, 'requirements.txt'), encoding='utf-8') as f:
     requirements = list(filter(None, f.read().split('\n')))
+
+# test requirements
+with open(
+        os.path.join(root_path, 'test_requirements.txt'),
+        encoding='utf-8') as f:
+    test_requirements = list(filter(None, f.read().split('\n')))
 
 setup(
     name='bot-calendario-telegram',
@@ -24,4 +51,7 @@ setup(
     author_email='luislivilla@gmail.com',
     license="GPLv3",
     packages=find_packages(exclude=['docs', 'tests', 'data']),
-    install_requires=requirements, )
+    install_requires=requirements,
+    tests_require=test_requirements,
+    cmdclass={'test': PyTest},
+)
