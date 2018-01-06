8fd6ff76d8a4dcc4cc60358246d3719a98e003de
lulivi <luislivilla@gmail.com>
2018-01-06 02:53:27 +0100

    Provisioning script

---
+++ scripts/provisioning.sh
@@ -0,0 +1,4 @@
+vagrantup --provider=azure
+
+fab -f ./deployment/fabfile.py -H vagrant@bot-calendario-telegram-vm.southcentralus.cloudapp.azure.com InstallApp
+fab -f ./deployment/fabfile.py -H vagrant@bot-calendario-telegram-vm.southcentralus.cloudapp.azure.com StartApp
