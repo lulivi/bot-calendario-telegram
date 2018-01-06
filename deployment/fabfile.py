cc07cfa6de8a859dbae6839e2092e710477fda02
lulivi <luislivilla@gmail.com>
2018-01-06 01:59:49 +0100

    Added fabric deployment instructions

---
+++ deployment/fabfile.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Deployment of the app.
+
+Copyright 2017, Luis Liñán (luislivilla@gmail.com)
+
+This program is free software: you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation, version 3.
+
+This program is distributed in the hope that it will be
+useful, but WITHOUT ANY WARRANTY; without even the implied
+warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+PURPOSE. See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>
+"""
+from fabric.api import run
+
+
+def InstallApp():
+    # Download the repository
+    run('git clone https://github.com/lulivi/bot-calendario-telegram.git')
+
+    # Install the app requirements
+    run('cd bot-calendario-telegram/ && pip3 install -r requirements.txt')
+
+
+def RemoveApp():
+    # Delete the repository
+    run('sudo rm -rf ./bot-calendario-telegram')
+
+
+def StartApp():
+    # Start web service
+    run('cd bot_calendario_telegram/ && hug -p 80 -f reminder_rest_api.py',
+        pty=False)
