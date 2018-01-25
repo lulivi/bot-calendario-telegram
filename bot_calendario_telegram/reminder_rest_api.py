ac781559592ad60558fd016d633b6832ef83fd46
lulivi <luislivilla@gmail.com>
2018-01-25 18:02:06 +0100

    Added new routes

--- bot_calendario_telegram/reminder_rest_api.py
+++ bot_calendario_telegram/reminder_rest_api.py
@@ -1,73 +1,148 @@
-# -*- coding: utf-8 -*-
-"""
-Provide responses for some routes in the app.
+"""Provide responses for some routes in the app."""
 
-Copyright 2017, Luis Liñán (luislivilla@gmail.com)
-
-This program is free software: you can redistribute it and/or
-modify it under the terms of the GNU General Public License
-as published by the Free Software Foundation, version 3.
-
-This program is distributed in the hope that it will be
-useful, but WITHOUT ANY WARRANTY; without even the implied
-warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
-PURPOSE. See the GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program. If not, see <http://www.gnu.org/licenses/>
-"""
-
-# External imports
+# System imports
 import hug
+import peewee
 import json
+import os
+import sqlite3
+from urllib import parse
+
+# Local imports
 import context
-from bot_calendario_telegram.settings import TESTING_VARS
+from bot_calendario_telegram.settings import (
+    TESTING_VARS,
+    REMINDER_REST_API_PORT,
+    HEROKU_PG_DATABASE_URL,
+)
+from bot_calendario_telegram.reminder import Reminder
 
 assert context
 
+# Create the reminder object
+reminder = None
 
-with open(TESTING_VARS['REMINDER_DATA_FILE'], 'r') as file:
-    reminder_object = json.loads(file.read())
 
-
-# === Get `/` ===
+# === Get / ===
 @hug.get('/')
 def index():
     """Return web status."""
     return {'status': 'OK'}
 
 
-# === Get `/status` ===
+# === Get /status ===
 @hug.get('/status')
 def status():
     """Return web status."""
     return {'status': 'OK'}
 
 
-# === Get `/all` ===
-@hug.get('/all')
-def get_all():
-    """Return all evnets."""
-    return reminder_object['event']
-
-
-# === Get `/number` ===
-@hug.get('/number')
-def events_number():
+# === Get /event/count ===
+@hug.get('/event/count')
+def get_count():
     """Return the number of events."""
-    return len(reminder_object['event'])
+    return reminder.get_all_events_count()
 
 
-# === Get `/get/{id}` ===
-@hug.get('/get/{id}')
-def get_one(id: int):
-    """Return one event."""
-    ev = None
-    for rem in reminder_object['event']:
-        if rem['id_event'] == str(id):
-            ev = rem
-            break
-    if ev is None:
-        return {'id_event': None, "data_event": None}
+# === Get /event/get/{event_id} ===
+@hug.get('/event/get/{event_id}')
+def get_one(event_id: hug.types.text):
+    """Return an event from it id."""
+    return reminder.get_event(event_id=event_id)
+
+
+# === Get /event/get/{event_id} ===
+@hug.get('/event/next')
+def get_next():
+    """Return an event from it id."""
+    return reminder.next_event()
+
+
+# === Get /event/list ===
+@hug.get('/event/list')
+def get_all():
+    """Return all events."""
+    return reminder.get_all_events()
+
+
+# === Post /event/new ===
+@hug.post('/event/new')
+def new_event(body):
+    """Insert a new event in the database."""
+    return reminder.save_event(
+        event_id=body.get('event_id'),
+        reminder_datetime=body.get('reminder_datetime'))
+
+
+# === Put /event/update ===
+@hug.put('/event/update')
+def update_event(body):
+    """Update an event."""
+    return reminder.update_event(
+        event_id=body.get('event_id'),
+        new_reminder_datetime=body.get('reminder_datetime'),
+    )
+
+
+# === Put /event/delete/{event_id} ===
+@hug.delete('/event/delete/{event_id}')
+def delete_event(event_id: hug.types.text):
+    """Delete an event."""
+    return reminder.delete_event(event_id=event_id)
+
+
+def main():
+    """Main program function."""
+    global reminder
+
+    # Check if we are in production or in local testing
+    if HEROKU_PG_DATABASE_URL:
+        # Obtain the database url and split it into necesary parts
+        parse.uses_netloc.append('postgres')
+        db_url = parse.urlparse(HEROKU_PG_DATABASE_URL)
+
+        # Create the database object
+        database = peewee.PostgresqlDatabase(
+            db_name=db_url.path[1:],
+            user=db_url.username,
+            password=db_url.password,
+            host=db_url.hostname,
+            port=db_url.port)
     else:
-        return ev
+        with open(TESTING_VARS['REMINDER_DATA_FILE'], 'r') as f:
+            reminder_test_data = json.loads(f.read()).get('event_reminder')
+
+        test_data = [(k['event_id'], k['reminder_datetime'])
+                     for k in reminder_test_data]
+
+        # Createconnection and connect
+        conn = sqlite3.connect(TESTING_VARS['DB_NAME'])
+        c = conn.cursor()
+        # Create table
+        c.execute('DROP TABLE IF EXISTS \'event_reminder\'')
+        c.execute('CREATE TABLE event_reminder(id INTEGER PRIMARY KEY, \
+event_id varchar(255) NOT NULL UNIQUE,reminder_datetime datetime NOT NULL);')
+        # Insert the test data into the database
+        c.executemany('INSERT INTO event_reminder (event_id, \
+reminder_datetime) VALUES (?, ?)', test_data)
+        # Save (commit) the changes and close
+        conn.commit()
+        conn.close()
+        database = peewee.SqliteDatabase(TESTING_VARS['DB_NAME'])
+
+    # Create the reminder object and initialize it with the database
+    reminder = Reminder()
+    reminder.initialize(database)
+
+    try:
+        # Start hug web rest api
+        hug.API(__name__).http.serve(REMINDER_REST_API_PORT)
+    except KeyboardInterrupt:
+        if TESTING_VARS['DB_NAME'] != ':memory:':
+            os.remove(TESTING_VARS['DB_NAME'])
+        reminder.close()
+        print('Rest api ended.')
+
+
+if __name__ == '__main__':
+    main()
